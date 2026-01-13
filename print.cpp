c948a66207426f89fe746ef7281114b9b0368d0b
Yuntaiwu <767306585@qq.com>
2026-01-13 13:47:38 +0800

    加了一行输出，只修改print，未修改readme

--- print.cpp
+++ print.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     cout << "Hello, World!" << endl;
     cout << "This project uses the C++17 language standard version." << endl;
+    cout << "只修改了print，未修改readme"<< endl;
     return 0;
 }
