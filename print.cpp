e721fa8298fcfe3ad89aca06ad7b25a246c2c55b
Yuntaiwu <767306585@qq.com>
2026-01-13 11:44:23 +0800

    重新书写了redme和print的内容

--- print.cpp
+++ print.cpp
@@ -2,7 +2,5 @@
 using namespace std;
 int main(){
     cout << "Hello, World!" << endl;
-    cout << "This project uses the C++17 language standard version." << endl;
-    cout<< "test"<< endl;
     return 0;
 }
