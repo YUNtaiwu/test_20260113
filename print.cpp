4440fec6773d36c40ab691c1bab2b76e1fefa27d
Yuntaiwu <767306585@qq.com>
2026-01-13 11:31:09 +0800

    增加输出test

--- print.cpp
+++ print.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     cout << "Hello, World!" << endl;
     cout << "This project uses the C++17 language standard version." << endl;
+    cout<< "test"<< endl;
     return 0;
 }
