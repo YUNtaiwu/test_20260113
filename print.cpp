a41fee0ceb64ed2bbcab8367bec4bad1fd5943c3
Yuntaiwu <767306585@qq.com>
2026-01-13 16:00:32 +0800

    删除print.cpp文件

--- print.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout << "Hello, World!" << endl;
-    cout << "This project uses the C++17 language standard version." << endl;
-    cout << "只修改了print，未修改readme"<< endl;
-    return 0;
-}
